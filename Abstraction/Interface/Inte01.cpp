339db9631a4a4f45773668b29c644160f3689ffa
thanhyou00 <truc06082000@gmail.com>
2021-10-10 12:46:00 +0700

    thanhyou00

--- Abstraction/Interface/Inte01.cpp
+++ Abstraction/Interface/Inte01.cpp
@@ -1,8 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+// Interface
 
+class IShape { // Khai bao 1 interface
+	public : 
+	virtual ~IShape(){}; // Ham huy ao (virtual destructor)
+	virtual void HelloInterface() = 0;
+	virtual void PrintName() = 0;
+};
+
+class Rectangle : IShape {
+	public : 
+		void HelloInterface() {
+			cout<<"Hello Interface !"<<endl;
+		};
+		void PrintName() {
+			cout<<"I'm thanhyou00"<<endl;
+		};
+		void PrintAge() {
+			cout<<"I'm 21 years old"<<endl;
+		};
+};
+
+int main() {
+	Rectangle rt;
+	rt.HelloInterface();
+	rt.PrintName();
+	rt.PrintAge();
 	return 0;
 }
 
